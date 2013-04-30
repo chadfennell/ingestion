60f2e53e1bfa9e96efdeb19776b56bb7fbb7c0ba
Szymon Guz <szymon@endpoint.com>
2013-04-30 19:44:06 +0200

    Moved all tests from selector to test/test_selector.

--- lib/selector.py
+++ lib/selector.py
@@ -71,36 +71,3 @@ def exists(obj,path):
 
     return found
 
-TEST_OBJ = {
-    'person': {
-        'name': 'john smith',
-        'age': 25
-    },
-    'address': {
-        'city': 'ottawa',
-        'street': '22 acacia'
-    },
-    'geek-code': 'GE d? s: a+'
-}
-def test_get():
-    assert getprop(TEST_OBJ,PATH_DELIM.join(('person','name'))) == TEST_OBJ['person']['name']
-    assert getprop(TEST_OBJ,PATH_DELIM.join(('person','age'))) == TEST_OBJ['person']['age']
-    assert getprop(TEST_OBJ,'address') == TEST_OBJ['address']
-    assert getprop(TEST_OBJ,'geek-code') == TEST_OBJ['geek-code']
-    assert getprop(TEST_OBJ,PATH_DELIM.join(('','person','name'))) == TEST_OBJ['person']['name']
-
-    assert exists(TEST_OBJ,PATH_DELIM.join(('person','name')))
-    assert exists(TEST_OBJ,PATH_DELIM.join(('person','age')))
-    assert exists(TEST_OBJ,'geek-code')
-    assert not exists(TEST_OBJ,'kajsdlj')
-    assert not exists(TEST_OBJ,PATH_DELIM.join(('person','sex')))
-
-def test_set_string():
-    o = copy.deepcopy(TEST_OBJ)
-    setprop(o,PATH_DELIM.join(('address','street')),'22 sussex')
-    assert getprop(o,PATH_DELIM.join(('address','street'))) == '22 sussex'
-
-def test_set_dict():
-    o = copy.deepcopy(TEST_OBJ)
-    setprop(o,'person',{'name': 'jason jones','age': 38})
-    assert getprop(o,PATH_DELIM.join(('person','age'))) == 38
